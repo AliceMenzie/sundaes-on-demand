1a1323a5018f429caa474db0d13b512fb248e43b
AliceMenzie <alice.menzie@gmail.com>
2022-01-17 17:20:25 +1100

    add styles for topping options

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div>
       < Options optionType='scoops'  />
+      < Options optionType='toppings'  />
       {/* < Options  /> */}
       < SummaryForm />
     </div>
