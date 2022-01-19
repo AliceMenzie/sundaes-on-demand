1503505910b1271e6333346f11de114a12348181
AliceMenzie <alice.menzie@gmail.com>
2022-01-19 15:43:38 +1100

    refactor currency formatter

---
+++ src/utilities/index.js
@@ -0,0 +1,9 @@
+// format number to currency
+
+export function formatCurrency(amount) {
+  return new Intl.NumberFormat("en-AU", {
+    style: "currency",
+    currency: "AUD",
+    minimumFractionDigits: 2,
+  }).format(amount);
+}
