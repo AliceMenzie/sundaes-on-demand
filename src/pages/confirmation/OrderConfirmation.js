723119ea2cc69250b6d4924d5e4e8b4a010447bb
AliceMenzie <alice.menzie@gmail.com>
2022-01-20 14:46:20 +1100

    add orderconfirmation logic

--- src/pages/confirmation/OrderConfirmation.js
+++ src/pages/confirmation/OrderConfirmation.js
@@ -1,47 +1,42 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import { useOrderDetails } from "../../contexts/OrderDetails"
+import axios from "axios";
+import Button from "react-bootstrap/Button";
+import { useEffect, useState } from "react";
+import { useOrderDetails } from "../../contexts/OrderDetails";
 
-export default function OrderConfirmation({setOrderPhase}) {
+export default function OrderConfirmation({ setOrderPhase }) {
+  const [orderNum, setOrderNum] = useState(null);
+  // below , , is ignoring the first two items and then destructing the third
+  const [, , resetOrder] = useOrderDetails();
 
-    const [orderNum, setOrderNum] = useState(null)
-    // below , , is ignoring the first two items and then destructing the third
-    const [, , resetOrder] = useOrderDetails
+  useEffect(() => {
+    axios
+      .post(`http://localhost:3030/order`)
+      .then((response) => {
+        setOrderNum(response.data.orderNumber);
+      })
+      .catch((error) => {
+        // TODO handle error
+      });
+  }, []);
 
-    useEffect(() => {
-        axios.post(`http://localhost:3030/order`)
-        .then((response) => {
-            setOrderNum(response.data.orderNumber)
-        })
-        .catch((error) => {
-            // TODO handle error
-        })
-    }, [])
+  function handleClick() {
+    // reset order is where the context comes in
+    resetOrder();
 
-    function handleClick() {
-        // reset order is where the context comes in 
-        resetOrder();
+    setOrderPhase("inProgress");
+  }
 
-        setOrderPhase('inProgress')
-    }
-
-    //  if orderNumber is falsey
-    if (orderNum){
+  //  if orderNumber is falsey
+  if (orderNum) {
     return (
-        <div>
-            <h1>Thanks you!</h1>
-            <p>Your order Number is </p>
-            <p>as per our T&Cs, nothing will happen now</p>
-            <button
-            onClick={handleClick}
-            >Create New Order</button>
-        </div>
-    )
-} else {
- return (
-     <div>
-         Loading
-     </div>
- )
-}
+      <div>
+        <h1>Thanks you!</h1>
+        <p>Your order Number is { orderNum } </p>
+        <p style={{ fontSize: '45%' }}>as per our T&Cs, nothing will happen now</p>
+        <Button onClick={handleClick}>Create New Order</Button>
+      </div>
+    );
+  } else {
+    return <div>Loading</div>;
+  }
 }
