076d0421792af6f679d371e35ee7889ecbc531b9
AliceMenzie <alice.menzie@gmail.com>
2022-01-25 12:06:43 +1100

    add scoop number validation

--- src/pages/entry/ScoopOption.js
+++ src/pages/entry/ScoopOption.js
@@ -1,12 +1,23 @@
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import { useState } from "react";
 
 export default function ScoopOptions({ name, imagePath, updateItemCount }) {
+  const [isValid, setIsValid] = useState(true);
 
   const handleChange = (e) => {
-    updateItemCount(name, e.target.value)
-  }
+    const currentValue = e.target.value;
+    updateItemCount(name, currentValue);
+
+    const currentValueFloat = parseFloat(currentValue);
+
+    setIsValid(
+      0 <= currentValueFloat &&
+        currentValueFloat < 10 &&
+        Math.floor(currentValueFloat) === currentValueFloat
+    );
+  };
 
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
@@ -15,14 +26,21 @@ export default function ScoopOptions({ name, imagePath, updateItemCount }) {
         src={`http://localhost:3030/${imagePath}`}
         alt={`${name} scoop`}
       />
-      <Form.Group controlId={`${name}-count`} as={Row} style={{ marginTop: '10px' }}>
-        <Form.Label column xs="6" style={{textAlign: 'right'}}>{name}</Form.Label>
-        <Col xs='5' style={{textAlign: 'left'}}>
-        <Form.Control
-         type="number"
-         defaultValue={0}
-         onChange={handleChange}
-         />
+      <Form.Group
+        controlId={`${name}-count`}
+        as={Row}
+        style={{ marginTop: "10px" }}
+      >
+        <Form.Label column xs="6" style={{ textAlign: "right" }}>
+          {name}
+        </Form.Label>
+        <Col xs="5" style={{ textAlign: "left" }}>
+          <Form.Control
+            type="number"
+            defaultValue={0}
+            onChange={handleChange}
+            isInvalid ={!isValid}
+          />
         </Col>
       </Form.Group>
     </Col>
